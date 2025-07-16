f52ca56260246ef008f1acf683568b7c99e94b84
zmavm <75528251+zmavm@users.noreply.github.com>
2025-07-16 17:15:55 +0800

    Add files via upload

---
+++ src/client/kotlin/name/modid/RedbeankillerClient.kt
@@ -0,0 +1,9 @@
+package name.modid
+
+import net.fabricmc.api.ClientModInitializer
+
+object RedbeankillerClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/name/modid/RedbeankillerDataGenerator.kt
@@ -0,0 +1,9 @@
+package name.modid
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object RedbeankillerDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
